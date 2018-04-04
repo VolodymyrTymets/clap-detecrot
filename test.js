842dbece436b96b10762ba1f2a2fc5e75cb30030
vova <volodymyrtymets@gmail.com>
2018-04-04 23:15:09 +0300

    add test

--- test.js
+++ test.js
@@ -7,5 +7,7 @@ try {
 } catch (err) {
   console.log('Error -> GPIO is not detected!!!');
 }
-
-out.writeSync(parseInt(process.argv[2]));
+console.log('was ->', out.readSync());
+console.log('will *->', out.readSync() ^ 1);
+console.log('will ->', parseInt(process.argv[2]));
+out.writeSync(parseInt(process.argv[2]);
