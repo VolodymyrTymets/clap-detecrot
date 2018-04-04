a23cdd042cd2e6880891bc642293ec7e8771636c
vova <volodymyrtymets@gmail.com>
2018-04-04 23:28:04 +0300

    code ref

--- test.js
+++ test.js
@@ -4,11 +4,12 @@ const Gpio = require('onoff').Gpio;
 const gpioNumber = process.argv[3] || 14;
 const out = new Gpio(parseInt(gpioNumber), 'out');
 
-console.log(`${ gpioNumber} was ->`, out.readSync());
+
 console.log('will *->', out.readSync() ^ 1);
 console.log('will ->', parseInt(process.argv[2]));
 out.writeSync(parseInt(process.argv[2]));
 
-while (true){
-
-}
+setInterval(function () {
+  console.log(`${ gpioNumber} was ->`, out.readSync());
+  out.writeSync(out.readSync() ^ 1);
+}, 2000);
