cafd94dcd0472880f48f50f95f69656ad5584f8f
vova <volodymyrtymets@gmail.com>
2018-04-11 22:12:44 +0300

    code ref

--- test.js
+++ test.js
@@ -8,10 +8,11 @@ const out = new Gpio(parseInt(gpioNumber), 'out');
 console.log('will *->', out.readSync() ^ 1);
 console.log('will ->', parseInt(process.argv[3]));
 //out.writeSync(parseInt(process.argv[3]));
-
+let val = 1;
 setInterval(function () {
   console.log(`${ gpioNumber} was ->`, out.readSync());
-  out.writeSync(out.readSync() ^ 1);
+  out.write(val, console.log);
+  val = val ? 0 : 1;
 }, 2000);
 
 //out.writeSync(parseInt(process.argv[2]));
