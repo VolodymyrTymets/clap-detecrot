b3d1ffddcad962e73cbb11ec9cbf7f5d72c14d21
vova <volodymyrtymets@gmail.com>
2018-04-11 22:15:50 +0300

    code ref

--- test.js
+++ test.js
@@ -1,18 +1,24 @@
+// const Gpio = require('onoff').Gpio;
+//
+//
+// const gpioNumber = process.argv[3] || 14;
+// const out = new Gpio(parseInt(gpioNumber), 'out');
+//
+//
+// console.log('will *->', out.readSync() ^ 1);
+// console.log('will ->', parseInt(process.argv[3]));
+// //out.writeSync(parseInt(process.argv[3]));
+// let val = 1;
+// setInterval(function () {
+//   console.log(`${ gpioNumber} was ->`, out.readSync());
+//   out.write(val, console.log);
+//   val = val ? 0 : 1;
+// }, 2000);
+//
+// //out.writeSync(parseInt(process.argv[2]));
+
+
 const Gpio = require('onoff').Gpio;
+const led = new Gpio(17, 'out');
 
-
-const gpioNumber = process.argv[3] || 14;
-const out = new Gpio(parseInt(gpioNumber), 'out');
-
-
-console.log('will *->', out.readSync() ^ 1);
-console.log('will ->', parseInt(process.argv[3]));
-//out.writeSync(parseInt(process.argv[3]));
-let val = 1;
-setInterval(function () {
-  console.log(`${ gpioNumber} was ->`, out.readSync());
-  out.write(val, console.log);
-  val = val ? 0 : 1;
-}, 2000);
-
-//out.writeSync(parseInt(process.argv[2]));
+led.writeSync(led.readSync() ^ 1);
