214b13c34aea0202f22949871b6c938024d22b30
vova <volodymyrtymets@gmail.com>
2018-04-11 22:17:27 +0300

    code ref

--- test.js
+++ test.js
@@ -1,24 +1,15 @@
-// const Gpio = require('onoff').Gpio;
-//
-//
-// const gpioNumber = process.argv[3] || 14;
-// const out = new Gpio(parseInt(gpioNumber), 'out');
-//
-//
-// console.log('will *->', out.readSync() ^ 1);
-// console.log('will ->', parseInt(process.argv[3]));
-// //out.writeSync(parseInt(process.argv[3]));
-// let val = 1;
-// setInterval(function () {
-//   console.log(`${ gpioNumber} was ->`, out.readSync());
-//   out.write(val, console.log);
-//   val = val ? 0 : 1;
-// }, 2000);
-//
-// //out.writeSync(parseInt(process.argv[2]));
-
-
 const Gpio = require('onoff').Gpio;
-const led = new Gpio(17, 'out');
 
-led.writeSync(led.readSync() ^ 1);
+
+const gpioNumber = process.argv[3] || 14;
+const out = new Gpio(parseInt(gpioNumber), 'out');
+
+
+console.log('will *->', out.readSync() ^ 1);
+console.log('will ->', parseInt(process.argv[3]));
+out.writeSync(parseInt(process.argv[3]));
+
+setInterval(function () {
+  console.log(`${ gpioNumber} was ->`, out.readSync());
+  out.writeSync(out.readSync() ^ 1);
+}, 2000);
